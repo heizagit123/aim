aeb2b3bd1877e40d702f988d45927feb0f12a2a9
Aswini <aswiniat15@gmail.com>
2023-07-03 00:52:23 +0400

    hiii

---
+++ schoolstore/schoolapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+
+class application(models.Model):
+    name=models.CharField(max_length=250)
+    dob= models.DateField(null=True,blank=True)
+    age=models.IntegerField(blank=True,null=True)
+    gender=models.CharField(max_length=250)
+    phone=models.CharField(max_length=250 )
+    email=models.EmailField(max_length=200)
+    address=models.CharField(max_length=250)
+    department= models.CharField(max_length=250)
+    course = models.CharField(max_length=250)
+    purpose = models.CharField(max_length=250)
+    materials = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
